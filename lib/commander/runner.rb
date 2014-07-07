fb51432315296be028c460ac8b008db65791a37f
jschmid1 <jschmid@suse.de>
2014-07-07 16:38:41 +0200

    refactor

--- lib/commander/runner.rb
+++ lib/commander/runner.rb
@@ -4,16 +4,14 @@ require 'commander/trello'
 require 'trello'
 module Commander
 
-  NAMES = YAML.load_file("#{File.dirname(__FILE__)}/../../config/free.yml")
-  NOTFREE = YAML.load_file("#{File.dirname(__FILE__)}/../../config/notfree.yml")
-  CONFIG = YAML.load_file("#{File.dirname(__FILE__)}/../../config/.trello.yml")
+    NAMES = YAML.load_file("#{File.dirname(__FILE__)}/../../config/free.yml")
+    CONFIG = YAML.load_file("#{File.dirname(__FILE__)}/../../config/.trello.yml")
 
   class Runner
 
     attr_accessor :options, :board
 
     def initialize(opts = nil)
-      import
       @options         = opts
       @options[:force] = opts[:force]
       @selected_commander = opts[:force] || select_commander
@@ -24,21 +22,20 @@ module Commander
     end
 
     def set_commander
-      refill_list
+      import
       find_card
       comment_on_card
       delete_assigned_members
-      add_member_to_card
+      # add_member_to_card
       build_new_hash
       delete_in_old_hash(@selected_commander)
-      write_to_file('free', NAMES.to_yaml) # twice?
-      write_to_file('free', NAMES.to_yaml)
-      puts "chose #{@selected_commander}"
+      write_to_file('free', NAMES.merge(@content).to_yaml)
+      puts "Chose #{@selected_commander}"
 
     end
 
     def select_commander
-      NAMES.keys.sample
+      Hash[NAMES.sort_by { |k, v| [v[:times_commander], v[:date]] }].keys.first
     end
 
     def delete_in_old_hash(commander)
@@ -46,31 +43,18 @@ module Commander
     end
 
     def build_new_hash
-      # logical error while forcing with -f
-      @variable_name = Hash.new
-      @new_value = NAMES[@selected_commander]['times_commander'] += 1
-      @stuff_to_write = { @selected_commander => { 'times_commander' => @new_value , 'trello_name' => NAMES[@selected_commander]['trello_name']} }
-      if NOTFREE
-        write_to_file('notfree', NOTFREE.merge(@stuff_to_write).to_yaml)
-      else
-        write_to_file('notfree', (@stuff_to_write).to_yaml)
-      end
+      count_up
     end
+
+    def count_up
+      @counter = NAMES[@selected_commander]['times_commander'] += 1
+      @content = { @selected_commander => { 'times_commander' => @counter , 'trello_name' =>  NAMES[@selected_commander]['trello_name'], 'date' => Time.now } }
+    end
+
 
     def write_to_file(filename, content)
       File.open("#{File.dirname(__FILE__)}/../../config/#{filename}.yml", 'w') do |f|
         f.write(content)
-      end
-    end
-
-    def refill_list
-      if !NAMES.any?
-        FileUtils.cp("#{File.dirname(__FILE__)}/../../config/notfree.yml", "#{File.dirname(__FILE__)}/../../config/free.yml")
-        File.open("#{File.dirname(__FILE__)}/../../config/notfree.yml", 'w') do |f|
-          f.write('')
-        end
-        puts 'refilled the list[debug]'
-        abort('i need to die until i know how to reset CONSTANTS.. need to')
       end
     end
 
@@ -83,11 +67,7 @@ module Commander
     end
 
     def find_member
-      if NAMES[@selected_commander]
-        @trello.find_member_by_username(NAMES[@selected_commander]['trello_name']) #replace when list is valid with @selcted_commander
-      else
-        @trello.find_member_by_username(NOTFREE[@selected_commander]['trello_name'])
-      end
+      @trello.find_member_by_username(NAMES[@selected_commander]['trello_name'])
     end
 
     def comment_on_card
@@ -97,9 +77,9 @@ module Commander
 
     def delete_assigned_members
       begin
-      @trello.list_of_assigned_members(@card).each do |x|
-      @trello.remove_member(@trello.find_member_by_username(x), @card)
-      end
+        @trello.list_of_assigned_members(@card).each do |x|
+          @trello.remove_member(@trello.find_member_by_username(x), @card)
+        end
       rescue
         puts 'nothing more to delete'
       end
@@ -110,12 +90,29 @@ module Commander
     end
 
     def show_hist(commander)
-      puts "#{commander} was #{NAMES[commander]['times_commander'] } times Commanding officer of the week." if NAMES[commander]
-      puts "#{commander} was #{NOTFREE[commander]['times_commander'] } times Commanding officer of the week." if NOTFREE[commander]
+      puts "#{commander} was #{@list[commander]['times_commander'] } times Commanding officer of the week."
     end
   end
 end
 
 
-# Change variable names
-# add delete method
+# choose based on time
+
+
+# def refill_list
+#   unless NAMES.any?
+#     FileUtils.cp("#{File.dirname(__FILE__)}/../../config/notfree.yml", "#{File.dirname(__FILE__)}/../../config/free.yml")
+#     File.open("#{File.dirname(__FILE__)}/../../config/notfree.yml", 'w') do |f|
+#       f.write('')
+#     end
+#     puts 'refilled the list[debug]'
+#     abort('i need to die until i know how to reset CONSTANTS.. need to')
+# end
+# end
+
+
+# def actually_write
+#   NOTFREE ? write_to_file('notfree', NOTFREE.merge(@content).to_yaml) : write_to_file('notfree', (@content).to_yaml)
+# end
+
+# NOTFREE = YAML.load_file("#{File.dirname(__FILE__)}/../../config/notfree.yml")
