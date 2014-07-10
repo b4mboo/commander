33d4740a10c28a9769bf7ab42a66132a2fb200c7
jschmid1 <jschmid@suse.de>
2014-07-10 16:30:51 +0200

    latest

--- lib/commander/runner.rb
+++ lib/commander/runner.rb
@@ -23,46 +23,30 @@ module Commander
       set_commander
     end
 
-    # Should update all vacations
-    def update_vacations
-      NAMES.keys.each do |x|
-        create_vacations(x)
-        proper_vacations(x) unless @vacations.empty? || NAMES[x][:vacations].empty? #can be deleted later
-      end
-    end
-
     def set_commander
       import
       find_card
       update_vacations
       select_commander if @options[:auto]
-      comment_on_card
-      delete_assigned_members
-      add_member_to_card
-      @forced = build_new_hash(@selected_commander, count_up, false, NAMES[@selected_commander][:vacations]) if @options[:force]#replace with @vacations [debugging]
-      write_to_file('free', NAMES.merge(@forced).to_yaml) if @options[:force]
+      # comment_on_card
+      # delete_assigned_members
+      # add_member_to_card
+      write_to_file('free', (@forced).to_yaml) if @options[:force]
+      # set new hash for chosen TODO
       puts "Chose: #{@selected_commander}" # debug
     end
 
-    # How to ignore optionals (change the e.g. last parameter)
-    def hash_scaffold(commander = @selected_commander, counter = count_up, vacation_flag = false, vacation_dates = @vacations)
-
-      { commander => {times_commander: counter,
-                      trello_name: NAMES[commander][:trello_name],
-                      date: Time.now,
-                      vacation: vacation_flag,
-                      tel_name: NAMES[commander][:tel_name],
-                      vacations: vacation_dates}}
+    # Should update all vacations
+    def update_vacations
+      NAMES.keys.each do |x|
+        create_vacations(x)
+        proper_vacations(x) unless @vacations.empty? #for real run
+      end
     end
 
-
-    def build_new_hash(commander, counter, vacation_flag, vacation_dates)
-      { commander => { times_commander:  counter,
-                       trello_name: NAMES[@selected_commander][:trello_name],
-                       date: Time.now,
-                       vacation: vacation_flag,
-                       tel_name: NAMES[@selected_commander][:tel_name],
-                       vacations: vacation_dates } }
+    # merge will cause old data to remain.. delete and build new
+    def delete_old_hash(commander)
+      NAMES.delete(commander)
     end
 
     def set_vacation_flag(commander, state)
@@ -73,11 +57,18 @@ module Commander
                                          date: NAMES[commander][:date],
                                          vacation: @state,
                                          tel_name: NAMES[commander][:tel_name],
-                                         vacations: NAMES[commander][:vacations] } }
+                                         vacations: @vacations } } # for real run
 
-      @foo = YAML.load_file("#{File.dirname(__FILE__)}/../../config/free.yml")
+      # have to delete the old hash and rebuild it from scratch because merge sucks
+      @foo = load_new_yaml
+      @foo.delete(commander)
       @merged_stuff = @foo.merge(@vacation_state)
       write_to_file('free', @merged_stuff.to_yaml)
+    end
+
+    #needed for reload, to get latest filedata
+    def load_new_yaml
+      YAML.load_file("#{File.dirname(__FILE__)}/../../config/free.yml")
     end
 
     # Diry workaround..
@@ -86,39 +77,17 @@ module Commander
     end
 
     def proper_vacations(commander)
-      a = NAMES[commander][:vacations].map {|x| x.split(' - ') } ## replace later with @vacations
+      a = @vacations.map {|x| x.split(' - ') }
       c = a.map { |x| x.map { |b| Date.parse(b) } }
       c.each do |x|
+        # Unfortunately writes only if @vacation matches.. should write @vacation to file
         set_vacation_flag(commander, 'true') if (x[0]..x[1]).cover?(Date.today)
       end
     end
 
-    # rarely used
-    def set_vacation_list(commander)
-      write_to_file('free', NAMES.merge(hash_scaffold(commander)).to_yaml)
-    end
 
-    def show_status(commander)
-      puts "#{commander} was #{NAMES[commander][:times_commander] } times Commanding officer of the week."
-      puts "#{commander} is currently on vacation" if NAMES[commander][:vacation]
-      NAMES[commander][:vacations].each { |x| puts x}
-    end
 
-    def select_commander
-      # sorting logic
-      @selected_commander = Hash[NAMES.select { |_, v| !v[:vacation] }].sort_by{ |_, v| v[:times_commander] }
-      if @selected_commander.count > 1
-        if @selected_commander[0][1][:times_commander] == @selected_commander[1][1][:times_commander]
-          @selected_commander = @selected_commander.sort_by { |_,v| v[:date] }[0][0]
-        else
-          @selected_commander = @selected_commander[0][0]
-        end
-      else
-        @selected_commander = @selected_commander[0][0]
-      end
-    end
-
-    # Klaus
+    # Klaus refactor later
     def create_vacations(commander)
       @vacations = []
       tn = Net::Telnet.new('Host' => 'present.suse.de', 'Port' => 9874, 'Binmode' => false)
@@ -151,13 +120,47 @@ module Commander
       tn.close
     end
 
+    def build_new_hash(commander, counter, vacation_flag, vacation_dates)
+      { commander => { times_commander:  counter,
+                       trello_name: NAMES[@selected_commander][:trello_name],
+                       date: Time.now,
+                       vacation: vacation_flag,
+                       tel_name: NAMES[@selected_commander][:tel_name],
+                       vacations: vacation_dates } }
+    end
+
+    def select_commander
+      # sorting logic
+      new = load_new_yaml
+      @selected_commander = Hash[new.select { |_, v| !v[:vacation] }].sort_by{ |_, v| v[:times_commander] }
+      if @selected_commander.count > 1
+        if @selected_commander[0][1][:times_commander] == @selected_commander[1][1][:times_commander]
+          @selected_commander = @selected_commander.sort_by { |_,v| v[:date] }[0][0]
+        else
+          @selected_commander = @selected_commander[0][0]
+        end
+      else
+        @selected_commander = @selected_commander[0][0]
+      end
+    end
+
+    def show_status(commander)
+      puts "#{commander} was #{NAMES[commander][:times_commander] } times Commanding officer of the week."
+      puts "#{commander} is currently on vacation" if NAMES[commander][:vacation]
+      NAMES[commander][:vacations].each { |x| puts x}
+    end
+
+    def forced
+      @forced = build_new_hash(@selected_commander, count_up, false, @vacations) if @options[:force]#replace with @vacations [debugging]
+    end
+
     def delete_assigned_members
       begin
         @trello.list_of_assigned_members(@card).each do |x|
-        @trello.remove_member(@trello.find_member_by_username(x), @card)
+          @trello.remove_member(@trello.find_member_by_username(x), @card)
         end
       rescue
-        puts 'nothing more to delete'
+        puts 'Noone assigned.'
       end
     end
 
