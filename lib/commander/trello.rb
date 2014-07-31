3b924f0e7138110920ebde2825c79597fc72e2aa
jschmid1 <jschmid@suse.de>
2014-07-31 13:55:43 +0200

    add documentation

--- lib/commander/trello.rb
+++ lib/commander/trello.rb
@@ -16,34 +16,42 @@ module Commander
 
     attr_accessor :board
 
+    # Loads the Configuration for Trello Auth
     def initialize
       configure_trello
     end
 
+    # Adds a member to a certain card
     def add_commander_to_card(commander, card)
       card.add_member(commander) if commander
     end
 
+    # Comments on a certain card
     def comment_on_card(commander, card)
       card.add_comment(commander) if commander
     end
 
+    # Finds a member by its username
     def find_member_by_username(username)
       @board.members.find { |m| m.username == username }
     end
 
+    # Lists all members who are assigned on a certain card
     def list_of_assigned_members(card)
       card.assignees.map(&:username)
     end
 
+    # Finds a member by its id
     def find_member_by_id(id)
       @board.members.find { |m| m.id == id }
     end
 
+    # Finds a card by its id
     def find_card_by_id(id)
       @board.cards.find{|c| c.short_id == id.to_i}
     end
 
+    # Configures Trello
     def configure_trello
       Trello.configure do |config|
         config.developer_public_key = Commander::CONFIG['consumerkey']
